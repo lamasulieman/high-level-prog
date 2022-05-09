012f215eeba2cce9e32b9306738350ca03d321be
lamasulieman <104361815+lamasulieman@users.noreply.github.com>
2022-05-09 09:46:42 +0200

    Update date1.cpp

--- date1.cpp
+++ date1.cpp
@@ -1,6 +1,6 @@
 #include "std_lib_facilities.h"
 
-struct Date {      //creating a data type called date 
+struct Date {      //defining a data type called date 
     int y;
     int m;
     int d;
@@ -10,13 +10,13 @@ void init_day(Date& dd, int y, int m, int d)
 {
     if (m < 1 || m > 12) error("invalid month");
     if (d < 1 || d > 31) error("impossible day");
-    dd.y = y;
+    dd.y = y;           // to be able to edit them
     dd.m = m;
     dd.d = d;
 }
-void add_day(Date& dd, int n)
+void add_day(Date& dd, int n)       // fun that adds n days 
 {
-    // add n days 
+   
     dd.d += n;
     if (dd.d > 31) dd.d %= 31;
 }
